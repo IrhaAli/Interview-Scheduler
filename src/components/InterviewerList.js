8b8237ccd0f9dd1afd76b50d468bf0ed1ac0973b
Irha Ali <irhaali_97@hotmail.ca>
2023-02-22 15:47:04 -0500

    Complete InterviewerList Component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,22 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
 
 export default function InterviewList(props) {
   return (
-    <p>InterviewList</p>
-  )
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {props.interviewers.map((item) => (
+          <InterviewerListItem
+            id={item.id}
+            name={item.name}
+            avatar={item.avatar}
+            selected={props.interviewer === item.id}
+            setInterviewer={props.setInterviewer}
+          />
+        ))}
+      </ul>
+    </section>
+  );
 }
