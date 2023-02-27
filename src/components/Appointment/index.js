b5023fd362612a299ab758a7ddef9867da21c47d
Irha Ali <irhaali_97@hotmail.ca>
2023-02-27 16:39:59 -0500

    Error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "components/Appointment/styles.scss";
 import Header from './Header';
 import Show from './Show';
@@ -6,15 +6,20 @@ import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const [errorMessage, setErrorMessage] = useState("ERROR_MESSAGE");
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
   const DELETE = "DELETE";
   const DELETING = "DELETING";
+  const EDIT = "EDIT";
+  const ERROR = "ERROR";
+  const ERROR_MESSAGE = `Could not ${errorMessage} appointment`;
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -26,17 +31,26 @@ export default function Appointment(props) {
 
   function save(interviewDetails = null) {
     const interview = (interviewDetails) ? { student: interviewDetails.student, interviewer: interviewDetails.interviewer } : null;
-
-    transition((interviewDetails) ? SAVING : DELETING);
+    const action = (mode === DELETE) ? 'delete' : (mode === EDIT) ? 'edit' : 'create';
+    transition((action === 'delete') ? DELETING : SAVING, true);
 
     props.bookInterview(props.id, interview)
       .then(() => {
         transition((interviewDetails) ? SHOW : EMPTY)
-      });
+      })
+      .catch(() => {
+        setErrorMessage(action);
+        console.log(ERROR_MESSAGE);
+        transition(ERROR, true);
+      })
   }
 
   function onDelete() {
     transition(DELETE);
+  }
+
+  function onEdit() {
+    transition(EDIT);
   }
 
 
@@ -46,18 +60,28 @@ export default function Appointment(props) {
       {mode === EMPTY && <Empty onAdd={() => showForm()} />}
       {mode === SAVING && <Status text={'Saving'} />}
       {mode === DELETING && <Status text={'Deleting'} />}
+      {mode === ERROR && <Error heading={'Error'} text={ERROR_MESSAGE} onClose={() => back()} />}
       {mode === DELETE && <Confirm text={'Are you sure you would like to delete?'} onConfirm={() => save()} onCancel={() => back()} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
           onDelete={onDelete}
+          onEdit={onEdit}
         />)}
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
           onCancel={() => back()}
           onSave={save}
+        />)}
+      {mode === EDIT && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+          onSave={save}
+          interviewer={props.interview.interviewer.id}
+          student={props.interview.student}
         />)}
     </article>
   );
